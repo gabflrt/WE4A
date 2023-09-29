edfdf481808c93f43568b2293662122ccd94a48e
Quentin1er <quentin.macullo@utbm.fr>
2023-09-29 16:43:35 +0200

    Base du site

--- index.php
+++ index.php
@@ -1,16 +1,32 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="fr">
 <head>
-  <meta charset="utf-8">
-  <title>Titre de la page</title>
-  <link rel="stylesheet" href="style.css">
-  <script src="script.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>Ma Boutique en Ligne</title>
 </head>
 <body>
-  <p>quelque chose</p>
-  <p>test</p>
-  ...
-  <!-- Le reste du contenu -->
-  ...
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Produits</a></li>
+                <li><a href="#">Panier</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section class="presentation">
+        <h1>Bienvenue dans Ma Boutique en Ligne</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consectetur suscipit lectus, et aliquam ex aliquam id.</p>
+        <a href="#" class="bouton">Découvrir nos produits</a>
+    </section>
+    <section class="produits">
+        <!-- Vous pouvez ajouter ici la liste de vos produits avec des images et des descriptions -->
+    </section>
+    <footer>
+        <p>&copy; 2023 Ma Boutique en Ligne</p>
+    </footer>
 </body>
 </html>
