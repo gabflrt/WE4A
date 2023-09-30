36ad5466ac99fa2dd9dc4ad8b421fab84b29f5b1
Quentin1er <quentin.macullo@utbm.fr>
2023-09-30 09:35:31 +0200

    Tentative défilement images

--- index.php
+++ index.php
@@ -12,33 +12,27 @@
     <?php include("./pagesParts/header.php"); ?>
     
     <section class="presentation">
-        <h1>Bienvenue dans Ma Boutique en Ligne</h1>
+        <h1>Bienvenue</h1>
         
-        <p>Bienvenue au <strong>Nom de votre restaurant</strong>, l'endroit où la cuisine française authentique 
-        rencontre les saveurs locales, fraîches et de qualité à un prix abordable pour toute la famille. 
+        <p>Au <strong>Nom de votre restaurant</strong>, l'endroit où la cuisine française authentique 
+        rencontre les saveurs locales, fraîches et de qualité à un prix abordable pour tous. 
         Notre établissement est un véritable joyau pour les amateurs de gastronomie française 
         et ceux qui recherchent une expérience culinaire mémorable.</p>
 
-        <p>Notre équipe de chefs talentueux s'engage à vous offrir une expérience gustative inoubliable 
-          en utilisant des ingrédients locaux soigneusement sélectionnés pour leur fraîcheur et leur qualité exceptionnelle.
-          Chaque plat que nous servons est préparé avec amour, créativité et un souci du détail inégalé, 
-          afin de vous faire vivre une véritable aventure culinaire française.</p>
+        <p>Que vous veniez pour un dîner romantique, une sortie en famille ou un repas entre amis, lorsque vous franchissez nos portes, 
+          vous serez accueilli par une ambiance chaleureuse et conviviale qui invite à la détente. 
+          Notre menu varié saura satisfaire tous les palais.</p>
 
-        <p>Lorsque vous franchissez nos portes, vous serez accueilli par une ambiance chaleureuse et conviviale 
-          qui invite à la détente en famille ou entre amis. Notre menu varié saura satisfaire tous les palais, 
-          des plats classiques français tels que le coq au vin et la bouillabaisse aux créations innovantes inspirées 
-          des saveurs locales.</p>
+    </section>
 
-        <p>Nous sommes fiers de proposer des tarifs abordables pour que chacun puisse profiter de notre cuisine exceptionnelle.
-         Chez <strong>Nom de votre restaurant</strong>, nous croyons que la bonne nourriture ne devrait pas être un luxe, 
-         mais une expérience accessible à tous.</p>
+    <link rel="stylesheet" href="styles/carrousel.css">
+    <div class="carrousel">
+        <img src="images/plat1.jpg" alt="Image 1">
+        <img src="images/plat2.jpg" alt="Image 2">
+        <img src="images/plat3.jpg" alt="Image 3">
+    </div>
 
-       <p>Que vous veniez pour un dîner romantique, une sortie en famille ou un repas entre amis, 
-        <strong>Nom de votre restaurant</strong> est l'endroit idéal pour découvrir la richesse de la cuisine française 
-        tout en savourant des produits locaux frais et de qualité. Rejoignez-nous et laissez-nous vous faire voyager 
-        à travers les délices de la France, le tout à un prix qui ne pèsera pas sur votre portefeuille. 
-        Nous avons hâte de vous accueillir et de vous faire vivre une expérience gastronomique exceptionnelle.</p>
-
+    <section class="presentation">
         <p>Que vous veniez pour un dîner romantique, une sortie en famille ou un repas entre amis, 
         <strong>Nom de votre restaurant</strong> est l'endroit idéal pour découvrir la richesse de la cuisine française 
         tout en savourant des produits locaux frais et de qualité. Rejoignez-nous et laissez-nous vous faire voyager 
@@ -46,6 +40,7 @@
         Nous avons hâte de vous accueillir et de vous faire vivre une expérience gastronomique exceptionnelle.</p>
 
         <a href="#" class="bouton">Découvrir nos produits</a>
+
     </section>
     <section class="produits">
         <!-- Vous pouvez ajouter ici la liste de vos produits avec des images et des descriptions -->
