3449e5b71da57c3cb72b24ff1869010d73b9476e
Quentin1er <quentin.macullo@utbm.fr>
2023-11-01 18:08:10 +0100

    Gestion catalogue

--- back_end/catalogues.php
+++ back_end/catalogues.php
@@ -20,6 +20,7 @@ if ($_SESSION['admin'] == 0) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/produits.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <title>Gestion catalogues</title>
 </head>
@@ -27,9 +28,45 @@ if ($_SESSION['admin'] == 0) {
 <body>
 
     <?php include("./back_header.php"); ?>
+
+    <div id="ajouttype">
+        <h1>Liste des types déjà existants</h1>
+        <?php
+        
+            $recup_type = $db->prepare("SELECT DISTINCT type FROM plats");
+            $recup_type->execute();
+            $type = $recup_type->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($type as $type2) {
+                echo $type2['type'], "\n";
+            }
+        ?>
+    </div>
+        
+
+        <h1>Ajout d'un nouveau type</h1>
+        <div class="presentation">
+        <div class="contenu_plat">
+        <form method="post" action="../includes/ajout_type.php" enctype="multipart/form-data">
+            <label for="Type">Nouveau type :</label>
+            <input type="text" name="type"><br>
+            <p>Ajout d'un premier produit pour l'enregistrement :</p><br>
+            <label for="nom">Nom :</label>
+            <input type="text" name="nom" value="nom"><br>
+            <label for="description">Description :</label>
+            <textarea name="description">description</textarea><br>
+            <label for="prix">Prix :</label>
+            <input type="number" name="prix" value="prix"><br>
+            <label for="chaud">Chaud :</label>
+            <input type="checkbox" name="chaud" value="1"><br>
+            <label for="image">Image :</label>
+            <input type="file" name="image"><br>
+            <input type="submit" value="Ajouter">
+        </form>
+        </div>
+        </div>
     
 
-    <?php include("./back_footer.php"); ?>
+<?php include("./back_footer.php"); ?>
 
     </body>
 </html>
---
+++ includes/ajout_type.php
@@ -0,0 +1,35 @@
+<?php
+
+include './database.php';
+
+if(isset($_POST['nom']) && isset($_POST['description']) && isset($_POST['type']) && isset($_POST['prix'])) {
+    $nom = filter_input(INPUT_POST, 'nom', FILTER_SANITIZE_STRING);
+    $description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_STRING);
+    $prix = filter_input(INPUT_POST, 'prix', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+    $type = filter_input(INPUT_POST, 'type', FILTER_SANITIZE_STRING);
+    $chaud = isset($_POST['chaud']) ? 1 : 0;
+
+    // Vérifier si une image a été téléchargée
+    if(isset($_FILES['image']) && $_FILES['image']['error'] == 0) {
+        $image = $_FILES['image']['tmp_name'];
+        $imageData = file_get_contents($image);
+    } else {
+        $image = "../images/defaut.jpg";
+        $imageData = file_get_contents($image);
+    }
+
+    // Insérer un nouveau plat dans la base de données
+    $insertPlatReq = $db->prepare("INSERT INTO plats (nom, description, prix, type, chaud, image) VALUES (:nom, :description, :prix, :type, :chaud, :image)");
+    $insertPlatReq->bindParam(':nom', $nom);
+    $insertPlatReq->bindParam(':description', $description);
+    $insertPlatReq->bindParam(':prix', $prix);
+    $insertPlatReq->bindParam(':type', $type);
+    $insertPlatReq->bindParam(':chaud', $chaud);
+    $insertPlatReq->bindParam(':image', $image);
+    $insertPlatReq->execute();
+
+    // Rediriger l'utilisateur vers la page de détails du nouveau plat
+    header("Location: ../back_end/catalogues.php");
+    exit();
+}
+?>
