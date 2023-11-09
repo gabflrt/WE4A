bdc650e0386aab6e38fac6bd8e81d203213a17b4
Matthieu Delisle <mdelisle1@hotmail.com>
2023-11-09 19:00:51 +0100

    catalogue part1

--- back_end/catalogues.php
+++ back_end/catalogues.php
@@ -29,20 +29,100 @@ if ($_SESSION['admin'] == 0) {
 
     <?php include("./back_header.php"); ?>
 
+    <style>
+    .type-box{
+        background-color: #f2f2f2;
+        border: 1px solid #ccc;
+        padding: 20px;
+        margin-bottom: 10px;
+        border-radius: 5px;
+        box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
+    }
+
+    
+
+    .type-box .btn_rename {
+        background-color: #4CAF50;
+
+    }
+
+    .type-box .btn_delete {
+        background-color: #f44336;
+    }
+
+    .type-box .btn_rename:hover {
+        background-color: #45a049;
+    }
+
+    .type-box .btn_delete:hover  {
+        background-color: #8b0000;
+    }
+    </style>
     <div id="ajouttype">
         <h1>Liste des types déjà existants</h1>
         <?php
         
-            $recup_type = $db->prepare("SELECT DISTINCT type FROM plats");
+            $recup_type = $db->prepare("SELECT type FROM plats");
             $recup_type->execute();
-            $type = $recup_type->fetchAll(PDO::FETCH_ASSOC);
-            foreach ($type as $type2) {
-                echo $type2['type'], "\n";
-            }
+            $types = $recup_type->fetchAll(PDO::FETCH_ASSOC);
+
+        $type_counts = array_count_values(array_column($types, 'type'));
+        foreach ($type_counts as $type => $count) {
+            echo '<div class="type-box">';
+            echo '<h3 class="type-name">'.$type.'</h3> <br> <p>' . $count . ' produits </p><br>';
+            echo '<button class="btn_rename" onClick="renameCatalog(this)">Renommer</button>'; 
+            echo '<button class="btn_delete" onClick="deleteCatalog(this)">Supprimer</button>';            
+            echo '</div>';
+        }
+        
         ?>
         <br>
         <br>
-        
+
+        <script>
+    function renameCatalog(button) {
+        var typeBox = button.parentNode;
+        var typeHeader = typeBox.querySelector('.type-name');
+
+        if (!typeHeader.querySelector('input')) {
+            var currentName = typeHeader.innerText;
+            var inputField = document.createElement('input');
+            inputField.type = 'text';
+            inputField.value = currentName;
+            typeHeader.innerHTML = '';
+            typeHeader.appendChild(inputField);
+            inputField.focus();
+            inputField.setSelectionRange(0, inputField.value.length); 
+            
+            button.removeEventListener('click', renameCatalog);
+            button.addEventListener('click', function() {
+                var newName = inputField.value.trim();
+                if (newName !== '') {
+                    typeHeader.innerText = newName;
+                    //AJAX call ici et update en BDD
+                } else {
+                    typeHeader.innerText = currentName;
+                } 
+                button.removeEventListener('click', arguments.callee);
+                button.addEventListener('click', renameCatalog);
+            });
+
+            inputField.addEventListener('blur', function() {
+                inputField.focus();
+            });
+        }
+    }
+
+    function deleteCatalog(button) {
+        var typeBox = button.parentNode;
+        var typeToDelete = typeBox.querySelector('.type-name').innerText;
+
+        if (confirm('Voulez-vous vraiment supprimer le type suivant :  ' + typeToDelete + '?')) {
+            typeBox.remove();
+            //AJAX call ici et supprimer en BDD
+        }
+    }
+</script>
 
         <h1>Ajout d'un nouveau type</h1>
         <div class="presentation">
