4a84689d95adc35d30c5d12072c5814a2603dc0b
Gabriel Fleuret <gabfleuret@gmail.com>
2023-11-10 16:27:36 +0100

    get

--- back_end/inventaire.php
+++ back_end/inventaire.php
@@ -19,12 +19,8 @@ if ($_SESSION['admin'] == 0) {
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./styles/style.css">
-    <link rel="stylesheet" href="./styles/accueil.css">
-    <link rel="stylesheet" href="./styles/inventaire.css">
-
-
+    <link rel="stylesheet" href="../styles/inventaire.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 </head>
@@ -50,62 +46,13 @@ if ($_SESSION['admin'] == 0) {
                 echo "<p>L'ensemble des commandes du site</p>";
                 echo "<br>";
                 $chiffreAffaire=0;
-                $currentYear = date('Y'); 
-                $currentMonth = date('m'); 
-                $targetDate = $currentYear . '-' . $currentMonth . '-01';
+ 
+                $targetDate = date('Y-m-d', strtotime('-1 month'));
 
                 echo "<p id=chiffreAffaireDiv>Chiffre d'affaire depuis le ".$targetDate.": ".$chiffreAffaire." </p>";
-
+                
+                $date = date('2020-01-01');
                 include '../get_commandes.php'; 
-
-                foreach ($Commands as $command) {
-                    // Wrap each command in a styled container
-                    echo '<div class="command-container">';
-
-                    // Écrit les informations d'un client et le commandId
-                    echo '<button class="delete-button" onclick="confirmDelete(' . $command['id_commande'] . ')">Supprimer</button>';
-                    echo '<p class="info-row"><strong>Numéro commande :</strong> '. $command['id_commande'] .'</p>';
-                    echo '<p class="info-row"><strong>Client :</strong> ' . $command['clientInfo']['nom'] . ' ' . $command['clientInfo']['prenom'] . '</p>';
-                    echo '<p class="info-row"><strong>Date commande : </strong>'. $command['date_heure'] .'</p>';
-
-                    $prixTotal=0;
-                    echo '<p class="info-row"><strong>Détails commande</strong></p>';
-                    foreach ($command['commandInfo'] as $commandInfo) {
-                        $platId = $commandInfo['id_plat'];
-
-                        $PlatInfoQuery = $db->prepare("SELECT nom, prix FROM plats WHERE id_plat = :platId");
-                        $PlatInfoQuery->bindParam(':platId', $platId);
-                        $PlatInfoQuery->execute();
-                        $platInfo = $PlatInfoQuery->fetch();
-                        $prixTotal+= $platInfo['prix'];
-                        echo '<p class="info-row">' . $platInfo['nom'] . ' - ' . $platInfo['prix']. '€</p>';
-                    }
-
-                    foreach ($command['commandBoissonInfo'] as $commandInfo) {
-                        $boissonId = $commandInfo['id_boisson'];
-
-                        $BoissonInfoQuery = $db->prepare("SELECT nom, prix FROM boissons WHERE id_boisson = :boissonId");
-                        $BoissonInfoQuery->bindParam(':boissonId', $boissonId);
-                        $BoissonInfoQuery->execute();
-                        $BoissonInfo = $BoissonInfoQuery->fetch();
-                        $prixTotal+= $BoissonInfo['prix'];
-                        echo '<p class="info-row">' . $BoissonInfo['nom'] . ' - ' . $BoissonInfo['prix']. '€</p>';
-                    }
-
-                    echo '<p class="info-row">Prix total : '. $prixTotal . '€</p>';
-                    
-                    $dateCommand= date("Y-m-d", strtotime($command['date_heure']));
-                    $dateCommandStamp = strtotime($dateCommand);
-                    $targetDateStamp = strtotime($targetDate);
-                    if ($dateCommandStamp>$targetDateStamp){
-                        $chiffreAffaire+=$prixTotal;
-                        echo '<script>
-                        var chiffreAffaireDiv = document.getElementById("chiffreAffaireDiv");
-                        chiffreAffaireDiv.innerHTML = "Chiffre d\'affaire depuis le ' . $targetDate . ': ' . $chiffreAffaire . '€";
-                    </script>';
-                    }
-                    echo '</div>'; 
-                }
             }
             
         ?>
@@ -127,7 +74,7 @@ if ($_SESSION['admin'] == 0) {
             error: function (xhr, status, error) {
                 console.error(error);
             }
-        });
+        })
             }
 
         }
--- get_commandes.php
+++ get_commandes.php
@@ -1,6 +1,6 @@
 <?php
-
-$CommandsQuery = $db->prepare("SELECT * FROM commandes ORDER BY date_heure DESC");
+$CommandsQuery = $db->prepare("SELECT * FROM commandes WHERE date_heure > :date ORDER BY date_heure DESC");
+$CommandsQuery->bindParam(':date', $date);
 $CommandsQuery->execute();
 $Commands = $CommandsQuery->fetchAll();
 
@@ -28,5 +28,54 @@ foreach ($Commands as &$command) {
     $ClientInfoQuery->execute();
     $clientInfo = $ClientInfoQuery->fetch();
     $command['clientInfo'] = $clientInfo;
+}
+
+foreach ($Commands as $command) {
+    // Wrap each command in a styled container
+    echo '<div class="command-container">';
+
+    // Écrit les informations d'un client et le commandId
+    echo '<button class="delete-button" onclick="confirmDelete(' . $command['id_commande'] . ')">Supprimer</button>';
+    echo '<p class="info-row"><strong>Numéro commande :</strong> '. $command['id_commande'] .'</p>';
+    echo '<p class="info-row"><strong>Client :</strong> ' . $command['clientInfo']['nom'] . ' ' . $command['clientInfo']['prenom'] . '</p>';
+    echo '<p class="info-row"><strong>Date commande : </strong>'. $command['date_heure'] .'</p>';
+
+    $prixTotal=0;
+    echo '<p class="info-row"><strong>Détails commande</strong></p>';
+    foreach ($command['commandInfo'] as $commandInfo) {
+        $platId = $commandInfo['id_plat'];
+
+        $PlatInfoQuery = $db->prepare("SELECT nom, prix FROM plats WHERE id_plat = :platId");
+        $PlatInfoQuery->bindParam(':platId', $platId);
+        $PlatInfoQuery->execute();
+        $platInfo = $PlatInfoQuery->fetch();
+        $prixTotal+= $platInfo['prix'];
+        echo '<p class="info-row">' . $platInfo['nom'] . ' - ' . $platInfo['prix']. '€</p>';
+    }
+
+    foreach ($command['commandBoissonInfo'] as $commandInfo) {
+        $boissonId = $commandInfo['id_boisson'];
+
+        $BoissonInfoQuery = $db->prepare("SELECT nom, prix FROM boissons WHERE id_boisson = :boissonId");
+        $BoissonInfoQuery->bindParam(':boissonId', $boissonId);
+        $BoissonInfoQuery->execute();
+        $BoissonInfo = $BoissonInfoQuery->fetch();
+        $prixTotal+= $BoissonInfo['prix'];
+        echo '<p class="info-row">' . $BoissonInfo['nom'] . ' - ' . $BoissonInfo['prix']. '€</p>';
+    }
+
+    echo '<p class="info-row">Prix total : '. $prixTotal . '€</p>';
+    
+    $dateCommand= date("Y-m-d", strtotime($command['date_heure']));
+    $dateCommandStamp = strtotime($dateCommand);
+    $targetDateStamp = strtotime($targetDate);
+    if ($dateCommandStamp>=$targetDateStamp){
+        $chiffreAffaire+=$prixTotal;
+        echo '<script>
+        var chiffreAffaireDiv = document.getElementById("chiffreAffaireDiv");
+        chiffreAffaireDiv.innerHTML = "Chiffre d\'affaire depuis le ' . $targetDate . ': ' . $chiffreAffaire . '€";
+    </script>';
+    }
+    echo '</div>'; 
 }
 ?>
